20b8a27f11d917e39977cb10ee80254639f39e8f
kebsixx <rijikanw@gmail.com>
2024-11-11 18:27:57 +0700

    add bufferutil and utf-8-validate

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
