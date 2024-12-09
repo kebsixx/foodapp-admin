8689537f0c30be827620cbf1db035fbbc89347d7
kebsixx <rijikanw@gmail.com>
2024-12-09 19:47:36 +0700

    first setup get category data

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import { getMonthlyOrders } from "@/actions/orders";
 import PageComponent from "./page-component";
+import { get } from "http";
+import { getCategoryData } from "@/actions/categories";
 
 const AdminDashboard = async () => {
   const monthlyOrders = await getMonthlyOrders();
+  const categoryData = await getCategoryData();
 
-  console.log(monthlyOrders);
+  console.log(categoryData);
 
   return <PageComponent monthlyOrders={monthlyOrders} />;
 };
