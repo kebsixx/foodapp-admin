e68d418319d3fc48544f672692642c54be14454d
kebsixx <rijikanw@gmail.com>
2024-12-16 21:31:43 +0700

    change download style in hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -142,9 +142,13 @@ export default function Home() {
                 </p>
               </MotionWrapper>
               <MotionWrapper delay={0.4}>
-                <Button size="lg" className="bg-[#7FCD91] hover:bg-[#A6B37D]">
-                  Download Now <ChevronRight className="ml-2 h-4 w-4" />
-                </Button>
+                <a className="group relative inline-block focus:outline-none focus:ring">
+                  <span className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-[#7FCD91] transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
+
+                  <span className="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75">
+                    Download
+                  </span>
+                </a>
               </MotionWrapper>
             </div>
 
