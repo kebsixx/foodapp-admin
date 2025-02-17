cc8ee6388fc36ee8830910835a0cb15c72875270
kebsixx <rijikanw@gmail.com>
2025-02-17 21:14:57 +0700

    change icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.jpg" type="image/jpg" sizes="any" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
