24c6066af351f9670f4c64fcedceeef8eb2f98bc
kebsixx <rijikanw@gmail.com>
2025-02-19 21:14:55 +0700

    delete jpg favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.jpg" type="image/jpg" sizes="any" />
-      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
